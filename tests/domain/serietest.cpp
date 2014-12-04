8c3abcb9b42dea16661481c05fe5202745613c40
Franck Arrecot <franck.arrecot@kdab.com>
2014-12-04 19:42:15 +0100

    Adding serie instanciation test.

--- tests/domain/serietest.cpp
+++ tests/domain/serietest.cpp
@@ -11,18 +11,32 @@ public:
     SerieTest();
 
 private Q_SLOTS:
-    void testCase1();
+    void ShouldInstanciateASerie();
 };
 
 SerieTest::SerieTest()
 {
 }
 
-void SerieTest::testCase1()
+void SerieTest::ShouldInstanciateASerie()
 {
     Domain::Serie serie;
-    //
-    QVERIFY2(true, "Failure");
+    serie.setName("Community");
+    serie.setAverageDuration(22);
+
+    serie.setYear(2009);
+    serie.setDiffusion(QDate(2009, 9, 17));
+
+    serie.setSleevePicture("resources/images/Game_of_Thrones_bandeau.jpg");
+    serie.setBannerPicture("resources/images/Game_of_Thrones_pochette.jpg");
+
+    QCOMPARE(serie.name(), QString("Community"));
+    QCOMPARE(serie.averageDuration(), 22);
+    QCOMPARE(serie.year(), 2009);
+    QCOMPARE(serie.diffusion(), QDate(2009, 9, 17));
+
+    QCOMPARE(serie.sleevePicture(), QString("resources/images/Game_of_Thrones_bandeau.jpg"));
+    QCOMPARE(serie.bannerPicture(), QString("resources/images/Game_of_Thrones_pochette.jpg"));
 }
 
 QTEST_APPLESS_MAIN(SerieTest)
